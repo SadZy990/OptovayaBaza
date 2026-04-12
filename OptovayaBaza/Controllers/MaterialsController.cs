bcfd73fcc13e4076495116f0f156f6bd7de719f6
SadZy990 <gnusarev_02@mail.ru>
2026-04-12 22:17:50 +0300

    Тесты log4net и описание для 4 лабораторной для Singelton

--- OptovayaBaza/Controllers/MaterialsController.cs
+++ OptovayaBaza/Controllers/MaterialsController.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using OptovayaBaza.Models;
 using OptovayaBaza.Helpers;
+using log4net;
 
 
 namespace OptovayaBaza.Controllers
 {
     public class MaterialsController : Controller
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(MaterialsController));
         private OptovayaBazaContext db;
         public MaterialsController()
         {
@@ -53,13 +55,16 @@ namespace OptovayaBaza.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Material material)
         {
+            log.Info($"Попытка добавления материала: {material.Name}");
             if (ModelState.IsValid)
             {
                 db.Materials.Add(material);
                 db.SaveChanges();
+                log.Info($"Материал успешно добавлен: ID={material.Id}, Name={material.Name}, Stock={material.Stock}");
                 TempData["Success"] = "Материал успешно добавлен!";
                 return RedirectToAction("Index");
             }
+            log.Warn($"Ошибка при добавлении материала: {material.Name}");
             return View(material);
         }
 
@@ -82,13 +87,16 @@ namespace OptovayaBaza.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Material material)
         {
+            log.Info($"Попытка редактирования материала ID={material.Id}, Name={material.Name}");
             if (ModelState.IsValid)
             {
                 db.Entry(material).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
+                log.Info($"Материал успешно обновлён: ID={material.Id}, NewStock={material.Stock}");
                 TempData["Success"] = "Материал успешно обновлён!";
                 return RedirectToAction("Index");
             }
+            log.Warn($"Ошибка при редактировании материала ID={material.Id}");
             return View(material);
         }
 
@@ -116,6 +124,7 @@ namespace OptovayaBaza.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+            log.Info($"Попытка удаления материала ID={id}");
             var material = db.Materials.Find(id);
 
             if (material == null)
@@ -123,16 +132,18 @@ namespace OptovayaBaza.Controllers
                 return HttpNotFound();
             }
 
-            // Проверка, есть ли операции с этим материалом
+            //Проверка, есть ли операции с этим материалом
             var hasTransactions = db.Transactions.Any(t => t.MaterialId == id);
             if (hasTransactions)
             {
+                log.Warn($"Невозможно удалить материал ID={id}, есть связанные операции");
                 TempData["Error"] = "Нельзя удалить материал, так как есть операции с ним!";
                 return RedirectToAction("Index");
             }
 
             db.Materials.Remove(material);
             db.SaveChanges();
+            log.Info($"Материал успешно удалён: ID={id}, Name={material.Name}");
             TempData["Success"] = "Материал успешно удалён!";
             return RedirectToAction("Index");
         }
--- OptovayaBaza/Global.asax.cs
+++ OptovayaBaza/Global.asax.cs
@@ -1,6 +1,8 @@
 using OptovayaBaza.Controllers;
 using System.Web.Mvc;
 using System.Web.Routing;
+using log4net;
+using log4net.Config;
 
 namespace OptovayaBaza
 {
@@ -8,6 +10,8 @@ namespace OptovayaBaza
     {
         protected void Application_Start()
         {
+            // Инициализация log4net из отдельного файла
+            XmlConfigurator.Configure(new System.IO.FileInfo(Server.MapPath("~/log4net.config")));
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
